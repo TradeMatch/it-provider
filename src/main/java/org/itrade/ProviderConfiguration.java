c9efd78bbb9f439e7c12aa60d81d014da203a209
Dmytro Podyachiy <dimapog@gmail.com>
2014-12-14 22:38:39 +0100

    Mongo auth

--- src/main/java/org/itrade/ProviderConfiguration.java
+++ src/main/java/org/itrade/ProviderConfiguration.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.UnknownHostException;
+import java.util.Arrays;
 
 @Configuration
 public class ProviderConfiguration {
@@ -20,9 +23,23 @@ public class ProviderConfiguration {
     @Value("${mongo.configuration.port:27017}")
     private int mongoPort;
 
+    // Auth
+    @Value("${mongo.db.name:provider}")
+    private String dbName;
+
+    @Value("${mongo.configuration.user:it-provider}")
+    private String mongoUser;
+
+    @Value("${mongo.configuration.password:}")
+    private String mongoPassword;
+
     @Bean
     public Mongo mongoDb() throws UnknownHostException {
-        return new MongoClient(mongoHost, mongoPort);
+        // Auth
+        MongoCredential credential = MongoCredential.createMongoCRCredential(mongoUser, dbName, mongoPassword.toCharArray());
+        MongoClient mongoAuthClient = new MongoClient(new ServerAddress(mongoHost, mongoPort), Arrays.asList(credential));
+        // Use auth client or simple client if no mongoUser is specified (tests)
+        return mongoUser != null ? mongoAuthClient : new MongoClient(mongoHost, mongoPort);
     }
 
     @Bean
@@ -36,8 +53,4 @@ public class ProviderConfiguration {
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         return objectMapper;
     }
-
-
-
-
 }
